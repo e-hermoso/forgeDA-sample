46fa28aa94c4904d0a317efd62071e157ea4667e
e-hermoso <erichermoso5@gmail.com>
2020-09-22 09:13:03 -0700

    changed the color for overlay back to red

--- wwwroot/js/ExtensionOverlay.js
+++ wwwroot/js/ExtensionOverlay.js
@@ -119,7 +119,7 @@ function Show2dCurve(viewer) {
     // It is a known issue with OpenGL core
     // try with Safari
     var _curveMaterial = new THREE.LineBasicMaterial({
-        color: new THREE.Color(#add8e6),
+        color: new THREE.Color(0xFF000),
         transparent: true,
         depthWrite: false,
         depthTest: false,
